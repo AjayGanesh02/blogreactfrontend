8074e35677e450e81cf6c37dececaeddaae70aef
Ajay Ganesh <ajganesh2002@gmail.com>
2022-06-12 19:54:19 -0400

    added to sidebar and started posts

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import './Sidebar.css';
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebarItem'>
+            <span className='sidebarTitle'>ABOUT ME</span>
+            <img
+                src=''
+                alt=''
+            />
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+        </div>
+    </div>
+  )
+}
